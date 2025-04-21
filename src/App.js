389d3d11c75e6b670151c77328e75bf02bf4bfdc
Abdulwahab Alsager <abdulwahabalsager@gmail.com>
2025-04-21 11:18:18 +0300

    Added name, introduction, etc.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // This imports the CSS to style your page
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* Replace this with your name and intro */}
+        <h1>Welcome to Abdulwahab Alsager's Portfolio</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, I'm Abdulwahab! I'm a Software Engineer with a passion for problem-solving and creating user-centric applications. This site is part of my path to continue learning, expanding my skills, and showcasing some of my interests and projects. I hope you find value in it. Thank you for visiting!
+        </p>
+        <p>
+          Explore my work, including projects in React, Python, SQL, Tableau, and more!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/aalsager"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out my GitHub
         </a>
       </header>
     </div>
